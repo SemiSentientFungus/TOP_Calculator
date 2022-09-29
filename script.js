5e806c7e75066508bcdd8d8b052c4ff53816f43a
Semm <semisentientfungus@gmail.com>
2022-09-29 14:32:07 +0200

    create basic operate function

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
 //calling code:
-
+let sum = 0;
+operate("ivide", 2, 100);
+console.log(sum);
 //functions
 
-function add(a, b){
-    return a + b;
+function add(num1, num2){
+    sum = num1 + num2;
 }
 
-function subtract(a, b){
-    return a - b;
+function subtract(num1, num2){
+    sum = num1 - num2;
 }
 
-function multiply(a, b){
-    return a * b;
+function multiply(num1, num2){
+    sum = num1 * num2;
 }
 
-function divide(a, b){
-    return a / b;
+function divide(num1, num2){
+    sum = num1 / num2;
+}
+
+function operate(opp, num1, num2){
+    if (opp == "add"){
+        add(num1, num2);
+    }
+    else if (opp =="subtract"){
+        subtract(num1, num2);
+    }
+    else if (opp =="multiply"){
+        multiply(num1, num2);
+    }
+    else if (opp =="divide"){
+        divide(num1, num2);
+    }
+    else{
+        console.log("not valid :c");
+    }
 }
