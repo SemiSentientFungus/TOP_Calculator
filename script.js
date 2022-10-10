9a45280afbfaeb01fc12f826c574b69b51d12643
Semm <semisentientfungus@gmail.com>
2022-10-10 12:06:00 +0200

    add math functionality

--- script.js
+++ script.js
@@ -1,44 +1,137 @@
-//calling code:
-let sum = '';
-const numPad = document.querySelectorAll(".num");
+//defining DOM elements
+const text = document.querySelector(".text")
+const numPad = document.querySelectorAll(".num")
+
+const add = document.querySelector(".add");
+const subtract = document.querySelector(".subtract");
+const multiply = document.querySelector(".multiply");
+const divide = document.querySelector(".divide");
+const equal = document.querySelector(".operate");
+const clear = document.querySelector(".clear");
+//DOM functionality 
 numPad.forEach((numPad) => {
     numPad.addEventListener('click', () => {
-        sum += numPad.textContent;
-        console.log(sum);
+        display += numPad.textContent;
+        text.textContent = display;        
     });
 });
-//functions
 
-function add(num1, num2){
+add.addEventListener("click", () =>{
+    
+    if (sum === 0){
+        sum = parseInt(display);
+        display = "";
+        text.textContent = display;
+    }
+    else {
+        secondNum = parseInt(display);
+        display = "";
+        text.textContent = display;
+        operate(sum, secondNum);
+        console.log(sum);
+    }
+    opp = "add";
+})
+subtract.addEventListener("click", () =>{
+    if (sum === 0){
+        sum = parseInt(display);
+        display = "";
+        text.textContent = display;
+    }
+    else {
+        secondNum = parseInt(display);
+        display = "";
+        text.textContent = display;
+        operate(sum, secondNum);
+        console.log(sum);
+    }
+    opp = "subtract";
+})
+multiply.addEventListener("click", () =>{
+    if (sum === 0){
+        sum = parseInt(display);
+        display = "";
+        text.textContent = display;
+    }
+    else {
+        secondNum = parseInt(display);
+        display = "";
+        text.textContent = display;
+        operate(sum, secondNum);
+        console.log(sum);
+    }
+    opp = "multiply";
+})
+divide.addEventListener("click", () =>{
+    if (sum === 0){
+        sum = parseInt(display);
+        display = "";
+        text.textContent = display;
+    }
+    else {
+        secondNum = parseInt(display);
+        display = "";
+        text.textContent = display;
+        operate(sum, secondNum);
+        console.log(sum);
+    }
+    opp = "divide";
+})
+equal.addEventListener("click", () =>{
+    secondNum = parseInt(display);
+    operate(sum, secondNum);
+    display = sum;
+    text.textContent = display;
+})
+
+clear.addEventListener("click", () => {
+    display = "";
+    text.textContent = display;
+    sum = 0;
+    secondNum = 0;
+    opp = "";
+})
+
+//variables
+let display = "";
+let sum = 0;
+let secondNum = 0;
+//functions
+function addFunc(num1, num2){
     sum = num1 + num2;
 }
 
-function subtract(num1, num2){
-    sum = num1 - num2;
+function subtractFunc(num1, num2){
+    sum = num1 -num2;    
 }
 
-function multiply(num1, num2){
-    sum = num1 * num2;
+function multiplyFunc(num1, num2){
+    sum = num1 * num2;    
 }
 
-function divide(num1, num2){
-    sum = num1 / num2;
+function divideFunc(num1, num2){
+    if ( num2 == 0){
+        alert("you can't divide by 0 idiot");
+    }
+    else {
+        sum = num1 / num2;
+    }
 }
 
-function operate(opp, num1, num2){
+function operate(num1, num2){
     if (opp == "add"){
-        add(num1, num2);
+        addFunc(num1, num2);
     }
-    else if (opp =="subtract"){
-        subtract(num1, num2);
+    else if (opp == "subtract"){
+        subtractFunc(num1, num2);
     }
-    else if (opp =="multiply"){
-        multiply(num1, num2);
+    else if (opp == "multiply"){
+        multiplyFunc(num1, num2);
     }
-    else if (opp =="divide"){
-        divide(num1, num2);
+    else if (opp =="divide") {
+        divideFunc(num1, num2);
     }
-    else{
-        console.log("not valid :c");
+    else {
+        console.log("invalid method");
     }
 }
